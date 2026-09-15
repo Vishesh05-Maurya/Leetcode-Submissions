71dda277463b2581959f3355a9bd215ea3ac5352
Vishesh Marya <visheshmaurya05@gmail.com>
2026-09-15 21:15:01 +0530

    Time: 0 ms (100%), Space: 43.4 MB (58.35%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,42 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-         int left=0;
-       
-    int right=nums.length-1;
-  while(left<=right){
-
-// in the first step we move all zero in the left most 
- if(nums[left]==0){
-    left++;
- }else if(nums[left]!=0 && nums[right]==0){
-        int temp=nums[left];
-        nums[left]=nums[right];
-        nums[right]=temp;
-        left++;
-        right--;
+       int low=0;
+       int  mid = 0;
+       int high=nums.length-1;
         
- }else{
-    right--;
- }
-}
- // [0,0,1,2,1];
-// in second step - now zero are in the left mose place 
-// after zero if we got 2 in left and 1 in right we swap it a
-int end=nums.length-1;
-int start=left;
-while(start<end){
-if(nums[start]==1){
-    start++;
-}else if(nums[start]==2 && nums[end]==1){
-    int temp=nums[start];
-    nums[start]=nums[end];
-    nums[end]=temp;
-    start++;
-    end--;
-}
-else
-    end--;
-  }
+        while(mid<=high){
+             if(nums[mid]==1){
+                mid++;
+             }
+             else if(nums[mid]==2){
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
+             }
+      else{
+        int temp=nums[low];
+        nums[low]=nums[mid];
+        nums[mid]=temp;
+        low++;
+        mid++;
+      }
+
+        }
+
 
         
     }
